438f46de09372d49981a414176ad8fbe1e303075
GabrielZacariasG <gabriel.zacariasg@imss.gob.mx>
2026-07-17 14:37:45 -0600

    Create RootLayout component with metadata

---
+++ app/layout.js
@@ -0,0 +1,13 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'Disponibilidad Presupuestal - HGZ No. 02',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
