5e6893e24b92fbf0bb6a02c7e841a4fef00b457c
GabrielZacariasG <gabriel.zacariasg@imss.gob.mx>
2026-07-21 09:53:38 -0600

    Add dynamic export to route.js

--- app/api/ultima-fecha/route.js
+++ app/api/ultima-fecha/route.js
@@ -1,6 +1,8 @@
 import { supabaseAdmin } from '../../../lib/supabaseAdmin';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   const { data, error } = await supabaseAdmin
     .from('disponibilidad_cuenta_dia')
