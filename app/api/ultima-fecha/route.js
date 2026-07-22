353ace5af6673f1fe10dfca2c8314c788f7ed3a7
GabrielZacariasG <gabriel.zacariasg@imss.gob.mx>
2026-07-22 12:54:59 -0600

    Change dynamic export to fetchCache in route.js

--- app/api/ultima-fecha/route.js
+++ app/api/ultima-fecha/route.js
@@ -1,7 +1,7 @@
 import { supabaseAdmin } from '../../../lib/supabaseAdmin';
 import { NextResponse } from 'next/server';
 
-export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
 
 export async function GET() {
   const { data, error } = await supabaseAdmin
